8ce6a016a09d52bea260563902cfa802916088c2
Lucas Saft Schemes <135075935+LucasSchemes@users.noreply.github.com>
2025-12-10 13:33:10 -0300

    Update gerarChaves.js

--- backend/gerarChaves.js
+++ backend/gerarChaves.js
@@ -7,7 +7,7 @@ const vapidKeys = webpush.generateVAPIDKeys();
 // Salva em um arquivo JSON
 fs.writeFileSync('chaves.json', JSON.stringify(vapidKeys, null, 2));
 
-console.log('✅ Chaves geradas com sucesso no arquivo chaves.json!');
+console.log('Chaves geradas com sucesso no arquivo chaves.json!');
 console.log('---');
 console.log('COPIE ESTA PUBLIC KEY PARA O SEU FRONTEND (App.vue):');
 console.log(vapidKeys.publicKey);
